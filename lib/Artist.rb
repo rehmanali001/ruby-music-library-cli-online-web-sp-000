ef690a4d3483eda1a2cf041f59ce46405e4c715b
Rehman Ali <rehmanali001@gmail.com>
2019-08-19 19:21:25 +0000

    Automatically backed up by Learn

--- lib/Artist.rb
+++ lib/Artist.rb
@@ -1,41 +1,47 @@
-class Artist 
+class Artist
+  extend Concerns::Findable
+  attr_accessor :name
 
-    extend Concerns::Findable
-    attr_accessor :name, :songs
-  
     @@all = []
-    
-  def initialize(name)
-    @name = name 
+
+    def initialize(name)
+
+     @name = name
     @songs = []
-  end 
-  
-  def save 
-    @@all << self 
-  end
-  
-  def self.all
-    @@all 
-  end
-  
-  def self.destroy_all
-    @@all.clear
-  end 
-  
-  def self.create(name)
-    new_artist = Artist.new(name)
-    new_artist.save
-    new_artist
-  end
-  
-  def add_song(song)
-    song.artist = self if song.artist == NIL
-    @songs << song if songs.none? {|song_1| song_1 == song}
-  end 
-  
-  def genres
-    genres = []
-    @songs.each {|song| genres << song.genre}
-    genres.uniq
-  end 
-end 
+   end
+
+     def self.all
+     @@all
+   end
+
+    def self.destroy_all
+    self.all.clear
+   end
+
+    def save
+    @@all << self
+   end
+
+    def self.create(name)
+    self.new(name).tap do|song|
+      song.save
+     end
+   end
+
+    def songs
+    @songs
+   end
+
+    def add_song(song)
+    song.artist = self unless song.artist == self
+    @songs << song unless @songs.include?(song)
+   end
+
+    def genres
+    genres = @songs.collect do |song|
+      song.genre
+      end
+      genres.uniq
+   end
+
+ end 
