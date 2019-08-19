ce672a31da73f4d47e89aad5d5a072644ea0dfb6
Rehman Ali <rehmanali001@gmail.com>
2019-08-19 19:21:55 +0000

    Automatically backed up by Learn

--- lib/Genre.rb
+++ lib/Genre.rb
@@ -1,40 +1,48 @@
-class Genre 
+class Genre
   extend Concerns::Findable
-  attr_accessor :name, :songs
-  
-  @@all = []
-  
-  def initialize(name)
-    @name = name 
+  attr_accessor :name
+
+    @@all = []
+
+   def initialize(name)
+    @name = name
     @songs = []
   end
-  
-  def self.all
-    @@all 
-  end
-  
-  def save 
+
+     def self.all
+     @@all
+   end
+
+    def self.destroy_all
+    self.all.clear
+   end
+
+    def save
     @@all << self
-  end 
-  
-  def self.destroy_all 
-    @@all.clear
-  end 
-  
-  def self.create(name)
-    new_genre = Genre.new(name)
-    new_genre.save
-    new_genre
-  end 
-  
-    def add_song(song)
-    song.genre = self if song.genre == nil
-    @songs << song if @songs.none? {|song_1| song_1 == song}
+   end
+
+    def self.create(name)
+    self.new(name).tap do|song|
+      song.save
+   end
+ end
+
+    def songs
+    @songs
   end
-  
-  def artists
-    artists = []
-    @songs.each {|song| artists << song.artist}
+
+
+   def add_song(song)
+    song.genre = self unless song.genre == self
+    @songs << song unless @songs.include?(song)
+  end
+
+
+   def artists
+    artists = @songs.collect do |song|
+      song.artist
+    end
     artists.uniq
-  end 
-end 
+  end
+
+ end 
