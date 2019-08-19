ee6cf7612017c2d2f3d9b51f4b751b7973295bcc
Rehman Ali <rehmanali001@gmail.com>
2019-08-19 19:24:56 +0000

    Automatically backed up by Learn

--- lib/concerns/findable.rb
+++ lib/concerns/findable.rb
@@ -3,10 +3,9 @@ module Concerns
    def find_by_name(name)
     all.find do |object|
         object.name == name
-    end
+   end
 
    def find_or_create_by_name(name)
     find_by_name(name) || self.create(name)
-  end
   end
 end
